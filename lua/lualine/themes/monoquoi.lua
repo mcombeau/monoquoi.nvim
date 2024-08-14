eafb0109b9c993757caad3ff9cc03f6a35848c20
mcombeau <mcombeau@student.42.fr>
2024-08-14 14:19:46 +0200

    chore: update lualine colors

--- lua/lualine/themes/monoquoi.lua
+++ lua/lualine/themes/monoquoi.lua
@@ -3,31 +3,31 @@ local colors = require 'monoquoi.colors'
 local monoquoi = {}
 
 monoquoi.normal = {
-    a = { bg = colors.orange.bright, fg = colors.black.dark, gui = 'bold' },
+    a = { bg = colors.grey.brighter, fg = colors.black.dark, gui = 'bold' },
     b = { bg = colors.black.bright, fg = colors.white.dim },
     c = { bg = colors.bg_statusline, fg = colors.white.dark },
 }
 
 monoquoi.insert = {
-    a = { bg = colors.green.bright, fg = colors.black.dark, gui = 'bold' },
+    a = { bg = colors.grey.dark, fg = colors.accent, gui = 'bold' },
     b = { bg = colors.black.bright, fg = colors.white.dim },
     c = { bg = colors.bg_statusline, fg = colors.white.dark },
 }
 
 monoquoi.command = {
-    a = { bg = colors.cyan.bright, fg = colors.black.dark, gui = 'bold' },
+    a = { bg = colors.black.dark, fg = colors.white.light, gui = 'bold' },
     b = { bg = colors.black.bright, fg = colors.white.dim },
     c = { bg = colors.bg_statusline, fg = colors.white.dark },
 }
 
 monoquoi.visual = {
-    a = { bg = colors.red.bright, fg = colors.black.dark, gui = 'bold' },
+    a = { bg = colors.accent, fg = colors.black.dark, gui = 'bold' },
     b = { bg = colors.black.bright, fg = colors.white.dim },
     c = { bg = colors.bg_statusline, fg = colors.white.dark },
 }
 
 monoquoi.replace = {
-    a = { bg = colors.magenta.bright, fg = colors.black.dark, gui = 'bold' },
+    a = { bg = colors.red.bright, fg = colors.black.dark, gui = 'bold' },
     b = { bg = colors.black.bright, fg = colors.white.dim },
     c = { bg = colors.bg_statusline, fg = colors.white.dark },
 }
